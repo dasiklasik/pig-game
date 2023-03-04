ab89caf9242c4c5e3fce9eaa3424175f964fbd69
dasiklasik <daryasamsonowicz@gmail.com>
2023-03-04 14:00:09 +0300

    fix: change roll the dice function

--- script.js
+++ script.js
@@ -5,12 +5,31 @@ const rollDice = () => {
   const diceNumber = Math.trunc(1 + Math.random() * 6)
   diceEl.src = `assets/dice-${diceNumber}.png`
   diceEl.classList.remove('hidden')
-  currentNumber = diceNumber
-  firstPlayerActive ? currentScore1El.textContent = diceNumber.toString()
-    : currentScore2El.textContent = diceNumber.toString()
+
+  if (diceNumber === 1) {
+    firstPlayerActive =  !firstPlayerActive
+    if (firstPlayerActive) {
+      player2.classList.remove('player--active')
+      player1.classList.add('player--active')
+      currentNumber = 0
+      currentScore2El.textContent = currentNumber
+    } else {
+      player1.classList.remove('player--active')
+      player2.classList.add('player--active')
+      currentNumber = 0
+      currentScore1El.textContent = currentNumber
+    }
+  } else {
+    currentNumber += diceNumber
+    firstPlayerActive ? currentScore1El.textContent = currentNumber.toString()
+      : currentScore2El.textContent = currentNumber.toString()
+  }
+
 }
 
 //Selected elements
+const player1 = document.querySelector('.player--0')
+const player2 = document.querySelector('.player--1')
 const score1El = document.getElementById('score--0')
 const score2El = document.getElementById('score--1')
 const currentScore1El = document.getElementById('current--0')
